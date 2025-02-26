49370aa072cd261c7b0a5787b804f6a16a6ed6d1
Liujiawangmath <15674760037@163.com>
2025-02-26 16:30:13 +0800

    update

--- fealpy/csm/elasticoplastic_lfem_2d_solver_test.py
+++ fealpy/csm/elasticoplastic_lfem_2d_solver_test.py
@@ -12,7 +12,7 @@ from fealpy.decorator import cartesian
 from fealpy.solver import cg, spsolve
 from fealpy.material.elastic_material import LinearElasticMaterial
 from fealpy.material.elastico_plastic_material import PlasticMaterial
-from fealpy.fem.elasticoplastic_integrator import TransitionElasticIntegrator 
+from fealpy.fem.elasticoplastic_integrator import ElasticoplasticIntegrator
 from fealpy.sparse import COOTensor
 import argparse
 # 平面应变问题
@@ -128,9 +128,12 @@ p = args.degree
 space = LagrangeFESpace(mesh, p=p, ctype='C')
 tensor_space = TensorFunctionSpace(space, shape=(-1, 2))
 gdof = space.number_of_global_dofs()
-pfcm = LinearElasticMaterial(name='E1nu0.3', 
+pf = LinearElasticMaterial(name='E1nu0.3', 
                                             elastic_modulus=1e5, poisson_ratio=0.3, 
                                             hypo='plane_stress', device=bm.get_device(mesh))
+pfcm = PlasticMaterial(name='E1nu0.3',
+                        elastic_modulus=1e5, poisson_ratio=0.3,
+                        yield_stress=50, hardening_modulus=0.0, hypo='plane_stress', device=bm.get_device(mesh))
 qf = mesh.quadrature_formula(q=tensor_space.p+3)
 bcs, ws = qf.get_quadrature_points_and_weights()
 max_increment = 20
@@ -138,7 +141,7 @@ tol = 1e-6
 max_iter = 10
 load_factor = 0.0
 load_max = 1.0
-delta_lambda = 0.05
+delta_lambda_load = 0.05
 # 材料参数
 E = 1e5
 nu = 0.3
@@ -148,13 +151,14 @@ NC = mesh.number_of_cells()
 NQ = len(ws)
 plastic_strain = bm.zeros((NC, NQ, 3),**kwargs)
 equivalent_plastic_strain = bm.zeros((NC, NQ),**kwargs)
+strain_total_e = bm.zeros((NC, NQ, 3),**kwargs)
 D_ep_global = pfcm.elastic_matrix()
 
 import os
 os.makedirs("./results", exist_ok=True)
 # 增量加载主循环
 for increment in range(max_increment):
-    load_factor = min(load_factor + delta_lambda, load_max)
+    load_factor = min(load_factor + delta_lambda_load, load_max) #TODO: 增量加载
     print(f"=== Increment {increment+1}, Load: {load_factor*100:.1f}% ===")
     
     # 牛顿迭代
@@ -163,7 +167,7 @@ for increment in range(max_increment):
     
     for iter in range(max_iter):
         # 组装系统
-        elasticintegrator= TransitionElasticIntegrator(D_ep_global, material=pfcm,space=tensor_space, 
+        elasticintegrator= ElasticoplasticIntegrator(D_ep_global, material=pfcm,space=tensor_space, 
                                     q=tensor_space.p+3, equivalent_plastic_strain=equivalent_plastic_strain)
         bform = BilinearForm(tensor_space)
         bform.add_integrator(elasticintegrator)
@@ -191,6 +195,7 @@ for increment in range(max_increment):
         F_int = lform.assembly()
         '''
         R = F_ext - F_int
+        print(R.min(), R.max())
         # 边界条件处理
         dbc = DirichletBC(space=tensor_space, 
                     gd=pde.dirichlet, 
@@ -200,14 +205,13 @@ for increment in range(max_increment):
         
         # 求解线性系统
         du = cg(K, R, maxiter=1000, atol=1e-14, rtol=1e-14)
-        print(du)
         uh += du
         
         # 本构积分更新
         yield_stress = sigma_y
-        success, plastic_strain, D_ep_global,equivalent_plastic_strain = elasticintegrator.constitutive_update(
-            uh, plastic_strain, pfcm,yield_stress=yield_stress)
-        
+        success, plastic_strain, D_ep_global,strain_total_e = elasticintegrator.constitutive_update(
+            uh, plastic_strain, pfcm,yield_stress=yield_stress,strain_total_e=strain_total_e)
+        strain_total_e = strain_total_e
         if not success:
             break
             
@@ -219,8 +223,8 @@ for increment in range(max_increment):
             break
             
     if not converged:
-        delta_lambda *= 0.5
-        print(f"Reducing step size to {delta_lambda:.3f}")
+        delta_lambda_load *= 0.5
+        print(f"Reducing step size to {delta_lambda_load:.3f}")
         continue
     # 保存结果
     save_results(mesh, uh, equivalent_plastic_strain, increment+1)  # 从1开始编号
--- fealpy/fem/elasticoplastic_integrator.py
+++ fealpy/fem/elasticoplastic_integrator.py
@@ -15,7 +15,7 @@ from fealpy.fem.utils import SymbolicIntegration
 from fealpy.fem.linear_elastic_integrator import LinearElasticIntegrator
 from fealpy.fem.linear_form import LinearForm
 
-class TransitionElasticIntegrator(LinearElasticIntegrator):
+class ElasticoplasticIntegrator(LinearElasticIntegrator):
     def __init__(self, D_ep, space, material, q, equivalent_plastic_strain, method=None):
         # 传递 method 参数并调用父类构造函数
         super().__init__(material, q, method=method)
@@ -36,14 +36,12 @@ class TransitionElasticIntegrator(LinearElasticIntegrator):
         cell2dof = space.cell_to_dof()
         uh = bm.array(uh,**kwargs)  
         tldof = space.number_of_local_dofs()
-        uh_cell = bm.zeros((NC, tldof)) # (NC, tldof)
-        for c in range(NC):
-            uh_cell[c] = uh[cell2dof[c]]
+        uh_cell = uh[cell2dof]
         qf = mesh.quadrature_formula(q=space.p+3)   
         bcs, ws = qf.get_quadrature_points_and_weights()
         # 计算应变
-        B = self.material.strain_matrix(True, gphi=space.grad_basis(bcs))
-        strain_total = bm.einsum('cqijk,ci->cqj', B, uh_cell)
+        B = self.material.strain_matrix(True, gphi=space.scalar_space.grad_basis(bcs))
+        strain_total = bm.einsum('cqij,cj->cqi', B, uh_cell)
         strain_elastic = strain_total - plastic_strain
 
         # 计算应力
@@ -51,12 +49,12 @@ class TransitionElasticIntegrator(LinearElasticIntegrator):
 
         # 组装内部力
         cm = mesh.entity_measure('cell')
-        F_int_cell = bm.einsum('q, c, cqijk,cqj->ci', 
+        F_int_cell = bm.einsum('q, c, cqij,cqi->cj', 
                              ws, cm, B, stress) # (NC, tdof)
         
         return F_int_cell
     
-    def constitutive_update(self, uh, plastic_strain_old, material,yield_stress):
+    def constitutive_update(self, uh, plastic_strain_old, material,yield_stress,strain_total_e) -> TensorLike:
         """执行本构积分返回更新后的状态"""
         # 计算试应变
         space = self.space
@@ -65,15 +63,15 @@ class TransitionElasticIntegrator(LinearElasticIntegrator):
         kwargs = bm.context(node)
         qf = mesh.quadrature_formula(q=space.p+3)
         bcs, ws = qf.get_quadrature_points_and_weights()
-        B = material.strain_matrix(True,gphi=space.grad_basis(bcs))
+        B = material.strain_matrix(True,gphi=space.scalar_space.grad_basis(bcs))
         uh = bm.array(uh,**kwargs)  
         tldof = space.number_of_local_dofs()
         NC = mesh.number_of_cells() 
         uh_cell = bm.zeros((NC, tldof),**kwargs) # (NC, tldof)
         cell2dof = space.cell_to_dof()
-        for c in range(NC):
-            uh_cell[c] = uh[cell2dof[c]]
-        strain_total = bm.einsum('cqijk,ci->cqj', B, uh_cell)
+        uh_cell = uh[cell2dof]
+        strain_total = bm.einsum('cqij,cj->cqi', B, uh_cell)
+        d_strain = strain_total - strain_total_e
         strain_trial = strain_total - plastic_strain_old
         # 弹性预测
         stress_trial = bm.einsum('cqij,cqi->cqj', material.elastic_matrix(), strain_trial)
@@ -86,28 +84,30 @@ class TransitionElasticIntegrator(LinearElasticIntegrator):
         # 塑性修正
         if bm.any(yield_mask):
             # 计算流动方向
-            n = s_trial / (sigma_eff[..., None] + 1e-12)
-            
+            n = material.df_dsigma(stress_trial)
+            fenzi = bm.einsum('...i,...ij,...j->...', n, material.elastic_matrix(), d_strain)
+            fenmu = bm.einsum('...i,...ij,...j->...', n, material.elastic_matrix(), n)
+            fenmu_inv = 1 / (fenmu+1e-16)
+            delta_lambda = fenzi * fenmu_inv
+            '''
             # 计算塑性乘子
             delta_gamma = (sigma_eff[yield_mask] - yield_stress) / (3*material.mu)
-            # 计算等效塑性应变增量
-            delta_peeq = delta_gamma * bm.sqrt(2/3)
-
-            # 累积到全局变量
-            self.equivalent_plastic_strain[yield_mask] += delta_peeq
+            '''
 
             # 更新塑性应变
             plastic_strain_new = plastic_strain_old.copy()
-            plastic_strain_new[yield_mask] += delta_gamma[:, None] * n[yield_mask]
+            delta_lambda_lodf = delta_lambda[...,None]
+            plastic_strain_new[yield_mask] += delta_lambda_lodf[yield_mask] * n[yield_mask]
             
             # 更新弹塑性矩阵
             D_ep = self.update_elastoplastic_matrix(material, n,  yield_mask)
              # 在更新D_ep后添加
             eigenvalues = bm.linalg.eigvalsh(D_ep)
             print("Max eigenvalue:", eigenvalues.max())
-            return True, plastic_strain_new, D_ep, self.equivalent_plastic_strain
+            strain_total_e = strain_total.copy()
+            return True, plastic_strain_new, D_ep, strain_total_e
         else:
-            return True, plastic_strain_old, material.elastic_matrix(),self.equivalent_plastic_strain
+            return True, plastic_strain_old, material.elastic_matrix(),strain_total_e
 
     def update_elastoplastic_matrix(self, material, n,  yield_mask):
         """正确的弹塑性矩阵构造"""
@@ -116,13 +116,19 @@ class TransitionElasticIntegrator(LinearElasticIntegrator):
         # 计算分母项 H = n:D_e:n (标量)
         H = bm.einsum('...i,...ij,...j->...', n, D_e, n)
         H = H[..., None, None]
+        H_inv = 1 / (H + 1e-16)
         # 计算塑性修正项
+        num1 = bm.einsum('...ij,...j->...i', D_e, n)
+        num2 = bm.einsum('...i,...ij->...j', n, D_e)
+        numerator = bm.einsum('...i,...j->...ij', num1, num2)
+        '''
         numerator = bm.einsum('...i,...j->...ij', 
                             bm.einsum('...ik,...k', D_e, n),
                             bm.einsum('...jk,...k', D_e, n))
+        '''
         
         # 理想塑性时 H'=0
-        D_ep = D_e - numerator / (H + 1e-12)
+        D_ep = D_e - numerator * H_inv
         
         return bm.where(yield_mask[..., None, None], D_ep, D_e)
 
